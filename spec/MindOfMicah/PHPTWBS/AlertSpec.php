841a7ae65cdbe9c944b43d8bf66d98e573deb683
micah eschbacher <micah@whamdonk.com>
2014-07-13 13:18:34 -0400

    These tests were moved into the behat runner

--- spec/MindOfMicah/PHPTWBS/AlertSpec.php
+++ spec/MindOfMicah/PHPTWBS/AlertSpec.php
@@ -12,21 +12,6 @@ class AlertSpec extends ObjectBehavior
         $this->shouldHaveType('MindOfMicah\PHPTWBS\Alert');
     }
 
-    public function it_should_create_an_success_alert_with_a_message()
-    {
-        $message = '<p>I like turtles</p>';
-        $this->setMessage($message)->shouldBe($this);
-        $this->setStyle('success')->shouldBe($this);
-        $this->flatten()->shouldBe('<div class="alert alert-success" role="alert"><p>I like turtles</p></div>');
-    }
-
-    public function it_can_create_all_the_different_alerts()
-    {
-        foreach(['info', 'success', 'danger', 'warning'] as $alert_type) {
-            $this::$alert_type('msg')->flatten()->shouldBe('<div class="alert alert-'.$alert_type . '" role="alert">msg</div>');
-        }
-    }
-
     public function it_should_treat_flatten_as_the_to_string_method()
     {
         $alert = new \MindOfMicah\PHPTWBS\Alert();
@@ -36,11 +21,17 @@ class AlertSpec extends ObjectBehavior
     }
     public function it_should_quickly_create_a_success_message()
     {
-        $this::success("Hey, we did it")->flatten()->shouldBe('<div class="alert alert-success" role="alert">Hey, we did it</div>');
+        $this->setMessage('Hey, we did it');
+        $this->setStyle('success');
+        
+        $this->flatten()->shouldBe('<div class="alert alert-success" role="alert">Hey, we did it</div>');
     }
 
     public function it_should_quickly_create_a_success_message_with_a_heading()
     {
-        $this::success("Hey, we did it")->withHeading('Success')->flatten()->shouldBe('<div class="alert alert-success" role="alert"><h4>Success</h4>Hey, we did it</div>');
+        $this->setMessage('Hey, we did it');
+        $this->setStyle('success');
+        $this->withHeading('Success');
+        $this->flatten()->shouldBe('<div class="alert alert-success" role="alert"><h4>Success</h4>Hey, we did it</div>');
     }
 }
